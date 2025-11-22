5c0ef2a47e8c655af3d679b473ee015bed8a6f4d
Dom Junior <dmkde@hotmail.com>
2025-11-21 22:59:27 -0300

    v2

---
+++ app.py
@@ -0,0 +1,24 @@
+# BIBLIOTECAS IMPORTADAS
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import  SQLAlchemy
+
+
+#INICIANDO O APP FLASK
+app=Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('inicial.html')
+
+@app.route('/pagar')
+def pagar():
+     return render_template('pagar.html')
+
+
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
+
